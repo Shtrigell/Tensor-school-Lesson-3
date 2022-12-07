255ea19176bb70795f7e963648aa399ed1b8f6f4
Shtrigell <119353277+Shtrigell@users.noreply.github.com>
2022-12-07 19:23:05 +0300

    Add files via upload

---
+++ Tensor_school_Lesson_3__4.py
@@ -0,0 +1,19 @@
+import math, cmath
+print("Введите коэффициенты уравнения")
+a=float(input("Коэффициент a "))
+b=float(input("Коэффициент b "))
+c=float(input("Коэффициент c "))
+print(f"Ваше уравнение: ",a,"x^2+",b,"x+",c,sep ="")
+discr = (b**2)-(4*a*c)
+print("Дискриминант равен: ",discr)
+if discr>0:
+      x_1 = float((-b+math.sqrt(discr))/2*a)
+      x_2 = float((-b-math.sqrt(discr))/2*a)
+      print("Ваши корни : X1 = ",x_1,"X2 = ",x_2)
+elif discr==0:
+    x = -b/(2*a)
+    print("X = ")
+else:
+    x_1 = complex((-b+cmath.sqrt(discr))/2*a)
+    x_2 = x_1.conjugate()
+    print(" Ваши корни : X1 = ",x_1,"X2 = ",x_2)
