b137bb157cd21763d58f9b038a8ddf96799c5bca
Shtrigell <119353277+Shtrigell@users.noreply.github.com>
2022-12-21 18:55:35 +0300

    Update Tensor_school_Lesson_3__3.py

--- Tensor_school_Lesson_3__3.py
+++ Tensor_school_Lesson_3__3.py
@@ -4,14 +4,14 @@ a=float(input("Коэффициент a "))
 b=float(input("Коэффициент b "))
 c=float(input("Коэффициент c "))
 print(f"Ваше уравнение: ",a,"x^2+",b,"x+",c,sep ="")
-discr = (b**2)-(4*a*c)
+discr = (b**2) - (4*a*c)
 print("Дискриминант равен: ",discr)
 if discr>0:
-      x_1 = float((-b+math.sqrt(discr))/2*a)
-      x_2 = float((-b-math.sqrt(discr))/2*a)
+      x_1 = float((-b+math.sqrt(discr)) / 2*a)
+      x_2 = float((-b-math.sqrt(discr)) / 2*a)
       print("Ваши корни : X1 = ",x_1,"X2 = ",x_2)
 elif discr==0:
-    x = -b/(2*a)
+    x = -b / (2*a)
     print("X = ")
 else:
   print("Корней нет")
